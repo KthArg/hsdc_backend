e5dfd57ba4a6656d06863ecf0c3e1854ee0ac0b2
KthArg <165839445+KthArg@users.noreply.github.com>
2025-08-19 01:40:02 -0700

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,7 +4,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:3000'],
+      origin: ['*'],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization'],
     },
